8276f89d2f9a6cdb3ed5235a39b2c485a61ad491
Nikolay Salinder <nikolay.salinder@gmail.com>
2019-05-13 02:33:34 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple starter template with gulp4, scss, autoprefixer, browser-sync, normalize.
+
+Install dependencies:
+`npm i`
+
+Run command:
+`gulp`
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+// const uglify = require('gulp-uglify')
+
+function style() {
+  return gulp.src('./scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concat('main.css'))
+      .pipe(autoprefixer({
+            browsers: [ 'cover 99.5%'],
+            cascade: false
+      }))
+      .pipe(uglify())
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./js/*.js').on('change', browserSync.reload);
+};
+
+gulp.task('default', gulp.series(watch));
